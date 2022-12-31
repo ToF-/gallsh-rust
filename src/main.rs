b9ac39f05fbf509b7227e9a089b782ba7c13cf34
Christophe Thibaut <cthibaut@octo.com>
2022-12-31 12:33:45 +0100

    first gtk window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
 use std::env;
+
+const  APP_ID: &str = "org.gtk_rs.gallsh";
 
 fn main() {
     let mut gallshdir = String::from("images/");
@@ -9,4 +13,16 @@ fn main() {
         }
     }
     println!("GALLSHDIR={gallshdir}");
+    
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("gallsh")
+        .build();
+    window.present();
 }
