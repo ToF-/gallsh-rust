01478b48b8abef98f5529e16d80f0d4caed89b2b
Christophe Thibaut <cthibaut@octo.com>
2023-01-01 13:14:55 +0100

    no clue

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use gtk::prelude::*;
 use gtk::gdk;
 use gtk::glib;
 use glib::clone;
-use gtk::Application;
+use gtk::{Application, Label, WindowPosition};
 use gtk::gio;
 use gio::SimpleAction;
 
@@ -21,8 +21,8 @@ fn main() {
         .build();
 
     app.connect_activate(build_ui);
-    app.set_accels_for_action("win.close", &["q"]);
-    app.set_accels_for_action("action.next", &["n"]);
+    // app.set_accels_for_action("win.close", &["q"]);
+    app.set_accels_for_action("action.next", &["q"]);
     app.run();
 }
 
@@ -33,20 +33,37 @@ fn build_ui(app: &gtk::Application) {
         .title("gallsh")
         .build();
 
+    window.set_title("gallsh");
+    window.set_position(WindowPosition::Center);
+    window.set_size_request(400, 400);
+
+    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    let selected_index_label = Label::builder()
+        .label(&format!("selected index: {selected_index}"))
+        .build();
+
+    v_box.add(&selected_index_label);
+    window.add(&v_box);
+
     let action_next = SimpleAction::new_stateful(
         "next",
         Some(&u32::static_variant_type()),
         &selected_index.to_variant(),
         );
-    action_next.connect_activate(clone!(@weak window => move |action,_| {
+
+    action_next.connect_activate(clone!(@weak window => move |action,parameter| {
         let mut state = action
             .state()
             .expect("could not get state")
             .get::<u32>()
             .expect("the variant needs to be of type `u32`");
+        let parameter = parameter
+            .expect("could not get parameter")
+            .get::<u32>()
+            .expect("the variant needs to be of type `u32`");
         state += 1;
         action.set_state(&state.to_variant());
-        println!("selected index:{state}");
+        selected_index_label.set_label(&format!("selected index: {state}"));
     }));
 
     let action_close = SimpleAction::new("close", None);
@@ -54,7 +71,9 @@ fn build_ui(app: &gtk::Application) {
         window.close();
     }));
     window.add_action(&action_close);
+    window.add_action(&action_next);
 
+    window.show_all();
     window.present();
 }
 
