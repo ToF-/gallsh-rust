8a4025b77817e10660529aeb230050fad67494d8
Christophe Thibaut <cthibaut@octo.com>
2023-01-01 01:07:29 +0100

    detect key pressed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use gtk::prelude::*;
+use gtk::gdk;
+use gtk::glib;
 use gtk::Application;
 fn main() {
     let mut gallshdir = String::from("images/");
@@ -13,7 +15,7 @@ fn main() {
     let app = Application::builder()
         .application_id("org.example.gallsh")
         .build();
-    
+
     app.connect_activate(build_ui);
     app.run();
 }
@@ -23,6 +25,21 @@ fn build_ui(app: &gtk::Application) {
         .application(app)
         .title("gallsh")
         .build();
+
+    window
+    .connect("key_press_event", false, |values| {
+        let raw_event = &values[1].get::<gdk::Event>().unwrap();
+        match raw_event.downcast_ref::<gdk::EventKey>() {
+            Some(event) => {
+                println!("Key value: {:?}", event.keyval());
+                println!("Modifier: {:?}", event.state());
+            },
+            None => {},
+        }
+
+        let result = glib::value::Value::from_type(glib::types::Type::BOOL);
+        Some(result)
+    });
 
     window.present();
 }
