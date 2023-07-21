1c25677086c647327609d43d0314a361ef52c393
Christophe Thibaut <cthibaut@octo.com>
2023-07-21 21:40:43 +0200

    n and p display next and previous pictures

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
+use std::cell::Cell;
+use std::rc::Rc;
 use std::env;
 use std::io;
 use std::path::Path;
-use std::io::Error;
 use std::fs;
-use std::path::PathBuf;
 use glib::clone;
 use gtk::prelude::*;
-use gtk::{Application, gio, glib, Image };
+use gtk::{Application, gdk, gio, glib, Image, Window };
 use gio::SimpleAction;
 
 fn get_files_in_directory(dir_path: &str) -> io::Result<Vec<String>> {
-    // Get a list of all entries in the folder
     let entries = fs::read_dir(dir_path)?;
-
-    // Extract the filenames from the directory entries and store them in a vector
     let file_names: Vec<String> = entries
         .filter_map(|entry| {
             let path = entry.ok()?.path();
@@ -34,11 +31,23 @@ fn main() {
         .application_id("org.example.gallsh")
         .build();
 
+    app.connect_startup(|_| {
+        let provider = gtk::CssProvider::new();
+        provider.load_from_data("window { background-color:black;} image { margin:10em ; }");
+        gtk::style_context_add_provider_for_display(
+            &gdk::Display::default().unwrap(),
+            &provider,
+            1000,
+        );
+    });
+
     app.connect_activate(build_ui);
     app.set_accels_for_action("win.close", &["q"]);
     app.run();
 
     fn build_ui(app: &gtk::Application) {
+        let selected_image_index:Rc<Cell<usize>> = Rc::new(Cell::new(0));
+
         let mut gallshdir = String::from("images/");
         match env::var("GALLSHDIR")  {
             Ok(val) => gallshdir = String::from(val),
@@ -51,22 +60,7 @@ fn main() {
             Err(msg) => panic!("{}", msg),
             Ok(result) => result,
         };
-        //    let mut filenames = Vec::new();
-        //    let entries = fs::read_dir(gallshdir)?
-        //        .map(|result| result.map(|entry| entry.path()))
-        //        .collect::<Result<Vec<_>, io::Error>>()?;
-        //    dbg(entries);
-        //    for entry in entries {
-        //        let entry = entry?;
-        //        let filename = entry?.file_name();
-        //        filenames.push(filename.into_owned());
-        //        break;
-        //    }
-
-        let ref filename = filenames[0];
-        println!("reading image {}", filename);
-        let image = Image::from_file(filename);
-
+        let image = Image::new();
         let window = gtk::ApplicationWindow::builder()
             .application(app)
             .title("gsr")
@@ -75,14 +69,41 @@ fn main() {
             .child(&image)
             .build();
 
+        let index:usize = selected_image_index.get();
+        image.set_from_file(Some(&filenames[index]));
         let action_close = SimpleAction::new("close", None);
         action_close.connect_activate(clone!(@weak window => move |_, _| {
             window.close();
         }));
         window.add_action(&action_close);
         let evk = gtk::EventControllerKey::new();
-        evk.connect_key_pressed(|_, key, code, modifier_type| {
-            println!("Key:{}\nCode:{}\nModifier type:{}", key, code, modifier_type);
+        evk.connect_key_pressed(move |_, key, _, _| {
+            if let Some(s) = key.name() {
+                let mut current:usize = selected_image_index.get();
+                match s.as_str() {
+                    "n" => {
+                        if current == filenames.len() - 1 {
+                            current = 0;
+                        }
+                        else {
+                            current+= 1;
+                        }
+                        selected_image_index.set(current);
+                        let index:usize = selected_image_index.get();
+                        image.set_from_file(Some(&filenames[index]));
+                    },
+                    "p" => {
+                        if current == 0 {
+                            current = filenames.len();
+                        }
+                        current -= 1;
+                        selected_image_index.set(current);
+                        let index:usize = selected_image_index.get();
+                        image.set_from_file(Some(&filenames[index]));
+                    },
+                    _ => { },
+            };
+            }
             gtk::Inhibit(false)
         });
         window.add_controller(evk);
