54950b9f2ea4122ec4946fe4b0d2aa657850db2f
Christophe Thibaut <cthibaut@octo.com>
2023-07-21 16:35:43 +0200

    read the first file in the directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use std::env;
+use std::io;
+use std::path::Path;
+use std::io::Error;
 use std::fs;
 use std::path::PathBuf;
 use glib::clone;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, builders, EventControllerKey, gio, glib, gdk, Image };
+use gtk::{Application, gio, glib, Image };
 use gio::SimpleAction;
+
+fn get_files_in_directory(dir_path: &str) -> io::Result<Vec<String>> {
+    // Get a list of all entries in the folder
+    let entries = fs::read_dir(dir_path)?;
+
+    // Extract the filenames from the directory entries and store them in a vector
+    let file_names: Vec<String> = entries
+        .filter_map(|entry| {
+            let path = entry.ok()?.path();
+            if path.is_file() {
+                let full_path = Path::new(dir_path).join(path);
+                full_path.to_str().map(|s| s.to_owned())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(file_names)
+}
 
 fn main() {
     let app = Application::builder()
@@ -14,47 +37,57 @@ fn main() {
     app.connect_activate(build_ui);
     app.set_accels_for_action("win.close", &["q"]);
     app.run();
-}
 
-fn build_ui(app: &gtk::Application) {
-    let mut gallshdir = String::from("images/");
-    let selected_image_index = 0;
-    match env::var("GALLSHDIR")  {
-        Ok(val) => gallshdir = String::from(val),
-        Err(e) => {
-            println!("GALLSHDIR: {e}\n default to \"{gallshdir}\"");
-        }
-    };
-    let mut filename : String = "".to_string();
-    let entries = fs::read_dir(gallshdir).unwrap();
-    for entry in entries {
-        let path : PathBuf = entry.unwrap().path();
-        filename = String::from(path.to_str().unwrap());
-        break;
+    fn build_ui(app: &gtk::Application) {
+        let mut gallshdir = String::from("images/");
+        match env::var("GALLSHDIR")  {
+            Ok(val) => gallshdir = String::from(val),
+            Err(e) => {
+                println!("GALLSHDIR: {e}\n default to \"{gallshdir}\"");
+            }
+        };
+
+        let filenames = match get_files_in_directory(&gallshdir) {
+            Err(msg) => panic!("{}", msg),
+            Ok(result) => result,
+        };
+        //    let mut filenames = Vec::new();
+        //    let entries = fs::read_dir(gallshdir)?
+        //        .map(|result| result.map(|entry| entry.path()))
+        //        .collect::<Result<Vec<_>, io::Error>>()?;
+        //    dbg(entries);
+        //    for entry in entries {
+        //        let entry = entry?;
+        //        let filename = entry?.file_name();
+        //        filenames.push(filename.into_owned());
+        //        break;
+        //    }
+
+        let ref filename = filenames[0];
+        println!("reading image {}", filename);
+        let image = Image::from_file(filename);
+
+        let window = gtk::ApplicationWindow::builder()
+            .application(app)
+            .title("gsr")
+            .default_width(1000)
+            .default_height(1000)
+            .child(&image)
+            .build();
+
+        let action_close = SimpleAction::new("close", None);
+        action_close.connect_activate(clone!(@weak window => move |_, _| {
+            window.close();
+        }));
+        window.add_action(&action_close);
+        let evk = gtk::EventControllerKey::new();
+        evk.connect_key_pressed(|_, key, code, modifier_type| {
+            println!("Key:{}\nCode:{}\nModifier type:{}", key, code, modifier_type);
+            gtk::Inhibit(false)
+        });
+        window.add_controller(evk);
+
+        window.present();
     }
-
-    let image = Image::from_file(&filename);
-
-    let window = gtk::ApplicationWindow::builder()
-        .application(app)
-        .title(&filename)
-        .default_width(1000)
-        .default_height(1000)
-        .child(&image)
-        .build();
-
-    let action_close = SimpleAction::new("close", None);
-    action_close.connect_activate(clone!(@weak window => move |_, _| {
-        window.close();
-    }));
-    window.add_action(&action_close);
-    let evk = gtk::EventControllerKey::new();
-    evk.connect_key_pressed(|_, key, code, modifier_type| {
-        println!("Key:{}\nCode:{}\nModifier type:{}", key, code, modifier_type);
-        gtk::Inhibit(false)
-    });
-    window.add_controller(evk);
-
-    window.present();
 }
 
