0d1b0a90b7b352ebaa1215c2c3451682eb7acab8
ToF- <cthibauttof@gmail.com>
2024-05-27 16:49:28 +0200

    added tests for entry

--- src/entry.rs
+++ src/entry.rs
@@ -9,7 +9,7 @@ pub const IMAGE_DATA: &str = "IMAGE_DATA";
 
 pub type EntryList = Vec<Entry>;
 
-#[derive(Clone, Debug)]
+#[derive(PartialEq,Clone, Debug)]
 pub struct Entry {
     pub file_path: Rc<String>,
     pub file_size: u64,
@@ -109,3 +109,32 @@ impl Entry {
         original_file_name(&self.file_path)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::DateTime;
+
+    fn a_day() -> SystemTime {
+        DateTime::parse_from_rfc2822("Sun, 1 Jan 2023 10:52:37 GMT").unwrap().into()
+    }
+
+    #[test]
+    fn original_file_path_is_rid_of_any_thumb_suffix() {
+            let entry = make_entry(String::from("photos/fooTHUMB.jpeg"), 100, 5, a_day(), Rank::NoStar);
+            assert_eq!(String::from("photos/foo.jpeg"), entry.original_file_path());
+    }
+
+    #[test]
+    fn original_file_name_is_rid_of_any_thumb_suffixi_and_path() {
+            let entry = make_entry(String::from("photos/fooTHUMB.jpeg"), 100, 5, a_day(), Rank::NoStar);
+            assert_eq!(String::from("foo.jpeg"), entry.original_file_name());
+    }
+
+    #[test]
+    fn thumbnail_file_path_is_add_the_thumb_suffix() {
+            let entry = make_entry(String::from("photos/foo.jpeg"), 100, 5, a_day(), Rank::NoStar);
+            assert_eq!(String::from("photos/fooTHUMB.jpeg"), entry.thumbnail_file_path());
+    }
+}
+
--- src/repository.rs
+++ src/repository.rs
@@ -2,8 +2,10 @@ use crate::navigator::Navigator;
 use crate::entry::{EntryList, make_entry};
 use crate::rank::Rank;
 use std::time::SystemTime;
-use chrono::{prelude::*, Days, Months};
+use chrono::DateTime;
+use crate::Entry;
 
+#[derive(Debug)]
 pub struct Repository {
     pub entry_list: EntryList,
     pub navigator: Navigator,
@@ -11,11 +13,14 @@ pub struct Repository {
 
 impl Repository {
     pub fn from_entries(entries: EntryList, cells_per_row: usize) -> Self {
-        let entry_list = EntryList::new();
         Repository{
-            entry_list: entry_list,
+            entry_list: entries.clone(),
             navigator: Navigator::new(entries.len() as i32, cells_per_row as i32),
         }
+    }
+
+    pub fn entry(&self) -> Option<&Entry> {
+        Some(&self.entry_list[self.navigator.index()])
     }
 }
 #[cfg(test)]
@@ -32,10 +37,23 @@ mod tests {
             make_entry(String::from("photos/qux.jpeg"), 10, 25, day_c, Rank::TwoStars),
             make_entry(String::from("photos/bub.jpeg"), 100, 25, day_a, Rank::OneStar))
     }
+
     #[test]
-    fn creating_a_repository_from_a_list_of_entries() {
+    fn after_creating_a_repository_from_a_list_of_entries_entry_is_ref_to_first_entry() {
         let files = example_entry_list();
-        let repository = Repository::from_entries(files, 1);
+        let repository = Repository::from_entries(files.clone(), 2);
         assert_eq!(4, repository.navigator.capacity());
+        assert_eq!(2, repository.navigator.cells_per_row());
+        let entry: &Entry = repository.entry().unwrap();
+        assert_eq!(files.clone()[0], *entry);
+    }
+
+    #[test]
+    fn after_moving_one_col_entry_is_ref_to_second_entry() {
+        let files = example_entry_list();
+        let mut repository = Repository::from_entries(files.clone(), 2);
+        repository.navigator.move_rel((1,0));
+        let entry: &Entry = repository.entry().unwrap();
+        assert_eq!(files.clone()[1], *entry);
     }
 }
